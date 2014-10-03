87e09e1d098af0c7f3f37d7e782ec125fb2b83a0
Malcolm White <mcwhite@ucsd.edu>
2014-10-03 16:51:14 +0000

    Add a python script skeleton file.

---
+++ skeleton.py
@@ -0,0 +1,10 @@
+import sys
+
+def _parse_args()
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = _parse_args()
+    sys.exit(0)
